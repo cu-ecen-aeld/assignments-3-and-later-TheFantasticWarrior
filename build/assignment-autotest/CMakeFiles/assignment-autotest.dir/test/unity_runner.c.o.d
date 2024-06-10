f0a6dbc4ff255098f431e806d243ad0adb8dd98c
TheFantasticWarrior <tfwplssub@gmail.com>
2024-06-10 21:00:56 +0800

    remove build dir

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/tfw/coding/class/linux/assignment-1-TheFantasticWarrior/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
