61b5b1815f3ee06a15a15d01381cd8db247c12dd
TheFantasticWarrior <tfwplssub@gmail.com>
2024-06-14 21:29:13 +0800

    part 2

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
-if [[ ! -d $1 || ! $2 ]] ; then
+#!/bin/sh
+
+if [ ! -d "$1" ] || [ -z "$2" ]; then
     exit 1
 fi
-outputs=$(grep -hRc $1 -e $2)
-sum=($(awk '{n+=$0}{if ($0!=0){f++}} END {print n,f}' <<<"$outputs"))
-echo "The number of files are ${sum[1]} and the number of matching lines are ${sum[0]}"
+
+# Initialize counters
+num_files=0
+num_lines=0
+
+# Search for the pattern recursively in the specified directory
+find "$1" -type f -exec grep -q "$2" {} \; -exec grep -H -c "$2" {} + | {
+    while IFS=: read -r file_name count; do
+        # Increment file count
+        num_files=$((num_files + 1))
+        # Increment line count
+        num_lines=$((num_lines + count))
+    done
+
+    # Output results
+    echo "The number of files are ${num_files} and the number of matching lines are ${num_lines}"
+}
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,12 +35,23 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    git reset --hard ${KERNEL_VERSION}
+    make clean
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    cp $FINDER_APP_DIR/fixyylloc.patch .
+    git apply fixyylloc.patch
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    #make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+    echo kernel image made
 fi
-
+#: '
 echo "Adding the Image in outdir"
-
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
+
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
@@ -48,7 +59,10 @@ then
 fi
 
 # TODO: Create necessary base directories
-
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc lib home lib64 sbin sys usr tmp var proc
+mkdir -p usr/bin usr/lib usr/sbin var/log
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -56,25 +70,49 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+#'
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+TOOLCHAIN_LIBC="$(dirname $(which ${CROSS_COMPILE}gcc))/../aarch64-none-linux-gnu/libc/"
+files=$(aarch64-none-linux-gnu-readelf -a bin/busybox |grep "program interpreter"|sed -re "s/.*: ([^]]*).*/\1/")
 
+files=(${files} $(aarch64-none-linux-gnu-readelf -a bin/busybox |grep 'Shared library'|sed -re 's/.*\[([^]]*).*/lib64\/\1/'))
+if [ -z "${files}" ];then
+    exit 1
+fi
+echo ${files[@]}
+for file in "${files[@]}"; do
+    echo "$(pwd)/${file}"
+    cp "${TOOLCHAIN_LIBC}${file}" "$(pwd)/${file}"
+done
 # TODO: Add library dependencies to rootfs
 
 # TODO: Make device nodes
-
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 # TODO: Clean and build the writer utility
 
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
+scripts=(finder.sh writer conf finder-test.sh autorun-qemu.sh)
+cp -rL ${scripts[@]} "${OUTDIR}/rootfs/home/"
 # TODO: Chown the root directory
-
+#sudo chown -R root:root "${OUTDIR}/rootfs"
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > $OUTDIR/initramfs.cpio
+cd $OUTDIR
+gzip -f initramfs.cpio
